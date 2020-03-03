d3fb0d3ab5488428f0f4a50731fc192bc91742c9
plkujaw <jawor.jakub@gmail.com>
2020-03-03 12:28:03 +0000

    added and passed test for airport having a capacity that can be changed as appropriate

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,12 @@
 class Airport
 
+  attr_reader :capacity
+
+  CAPACITY = 100
+
+def initialize(capacity = CAPACITY)
+  @capacity = capacity
+
+end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,1 +1,9 @@
 require "airport"
+
+describe Airport do
+  subject(:airport) { described_class.new }
+
+  it "has a default capacity that can be overridden as appropriate" do
+    expect(subject.capacity).to eq(Airport::CAPACITY)
+  end
+end
